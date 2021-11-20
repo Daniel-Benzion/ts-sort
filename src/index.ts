9cb2acb0df455239d3c3154e8668ea1ac5362308
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-11-19 22:48:39 -0600

    Add LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 class Sorter {
 
-    constructor(public collection: number[]) {}
+    constructor(public collection: number[] | string | LinkedList) {}
 
     sort(): void {
         const { length } = this.collection;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
+
+                if (this.collection instanceof LinkedList) {
+                    
+                }
 
 
                 if (this.collection instanceof Array) {
@@ -18,7 +22,7 @@ class Sorter {
                 }
 
                if (typeof this.collection === 'string') {
-                   
+
                }
             }
         }
